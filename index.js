91b364efad2e63a694888334ac8e71e0c1b37ec4
OwO <41247578+youKnowOwO@users.noreply.github.com>
2018-09-29 08:17:07 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { readdirSync } = require('fs');
+const Client = require('./structures/Client.js');
+
+const client = new Client(process.env.TOKEN);
+
+for (const event of readdirSync('./events')) {
+	client.on(event.split('.')[0], require(`./events/${event}`).bind(null, client, ...args));
+}
+
+client.connect();
+
+process.on('error', e => console.error('[ERROR]', e));
+process.on('uncaughtException', e => console.error('[UNCAUGHT EXCEPTION]', e));
+process.en('unhandledRejection', e => console.error('[UNHANDLED REJECTION]', e));
